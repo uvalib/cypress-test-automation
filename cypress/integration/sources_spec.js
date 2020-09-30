de056427f01b5eed0b57e92ba66d6171eef0da0b
Dave Goldstein <dpg3k@virginia.edu>
2020-09-30 11:09:39 -0400

    Initial commit showing basic operation

---
+++ cypress/integration/sources_spec.js
@@ -0,0 +1,38 @@
+//
+// standard set of catalog URL's to verify the redirection javascript works correctly
+//
+
+const mustContain = 'Civil Air Patrol Searches for Plane Wreckage in West Virginia';
+const queryUrl    = 'sources/video/items/uva-lib:2214295';
+const searchUrl   = 'https://search.lib.virginia.edu';
+const v4Url       = 'https://v4.lib.virginia.edu';
+const kioskUrl    = 'https://search-kiosk.lib.virginia.edu';
+
+const test_A_Url  = searchUrl + '/' + queryUrl
+const test_B_Url  = v4Url     + '/' + queryUrl
+const test_C_Url  = kioskUrl  + '/' + queryUrl
+
+describe('Visit ' + test_A_Url, () => {
+  it('should visit ' + test_A_Url, () => { 
+    cy.visit(test_A_Url);
+    cy.contains(mustContain);
+  });
+});
+
+describe('Visit ' + test_B_Url, () => {
+  it('should visit ' + test_B_Url, () => { 
+    cy.visit(test_B_Url);
+    cy.contains(mustContain);
+  });
+});
+
+describe('Visit ' + test_C_Url, () => {
+  it('should visit ' + test_C_Url, () => { 
+    cy.visit(test_C_Url);
+    cy.contains(mustContain);
+  });
+});
+
+//
+// end of file
+//
