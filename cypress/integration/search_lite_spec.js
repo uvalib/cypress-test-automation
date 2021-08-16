df43f3c9707f7a9217b598cd37eddec9083535f9
Dave Goldstein <dpg3k@virginia.edu>
2021-08-16 09:03:39 -0400

    Add basic search test case

--- cypress/integration/search_lite_spec.js
+++ cypress/integration/search_lite_spec.js
@@ -8,9 +8,16 @@ const searchUrl   = 'https://virgo-lite-dev.lib.virginia.edu';
 describe('Visit search lite: ' + searchUrl, () => {
   it('should visit ' + searchUrl, () => { 
     cy.visit(searchUrl)
-    //cy.wait(2000)
     cy.get('uvalib-catalog-light').shadow().find('h1').contains('Search')
-    //cy.get('#q').type('jefferson{enter}')
+  });
+});
+
+describe('Search for "jefferson" on search lite: ' + searchUrl, () => {
+  it('should visit ' + searchUrl, () => {
+    cy.visit(searchUrl)
+    cy.get('uvalib-catalog-light').shadow().find('uvalib-page').find('uvalib-catalog-light-home').shadow().find('#search').type('jefferson{enter}')
+    cy.wait(2000)
+    cy.get('uvalib-catalog-light').shadow().find('uvalib-page').find('uvalib-catalog-light-home').shadow().find('uvalib-catalog-light-results')
   });
 });
 
