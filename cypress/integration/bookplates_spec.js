a612d3658aed75a9177a82e6375181ccef58da8d
Dave Goldstein <dpg3k@virginia.edu>
2022-01-26 09:54:47 -0500

    VIRGONEW-2411: bookplate query

--- cypress/integration/bookplates_spec.js
+++ cypress/integration/bookplates_spec.js
@@ -1,67 +1,26 @@
 //
-// standard set of catalog URL's to verify the redirection javascript works correctly
+// bookplate search from the main library site returns results
 //
 
 // test constants
-const queryUrl    = 'catalog?catalog_select=catalog&f[fund_code_facet][]=';
-const searchUrl   = 'https://search.lib.virginia.edu';
-const v4Url       = 'https://v4.lib.virginia.edu';
-const bookplate_1 = 'WEEDON-DR01378';
-const bookplate_2 = 'WEBB-ER00605';
-const bookplate_3 = 'TUNSTALL-ER00631';
+const libraryUrl = 'https://www.library.virginia.edu';
+const bookplate  = 'WEEDON-DR01378';
 
-const test_search_bp1_Url  = searchUrl + '/' + queryUrl + bookplate_1;
-const test_search_bp2_Url  = searchUrl + '/' + queryUrl + bookplate_2;
-const test_search_bp3_Url  = searchUrl + '/' + queryUrl + bookplate_3;
-const test_v4_bp1_Url      = v4Url     + '/' + queryUrl + bookplate_1;
-const test_v4_bp2_Url      = v4Url     + '/' + queryUrl + bookplate_2;
-const test_v4_bp3_Url      = v4Url     + '/' + queryUrl + bookplate_3;
+const test_start_Url  = libraryUrl + '/bookplates/' + bookplate;
 
-describe('Visit ' + test_search_bp1_Url, () => {
-  it('should visit ' + test_search_bp1_Url, () => { 
-    cy.visit(test_search_bp1_Url);
-    cy.contains( /results for:/ );
-    cy.contains( bookplate_1 );
-  });
-});
+describe('Visit ' + test_start_Url, () => {
+  it('should visit ' + test_start_Url, () => { 
+    cy.visit(test_start_Url);
 
-describe('Visit ' + test_search_bp2_Url, () => {
-  it('should visit ' + test_search_bp2_Url, () => { 
-    cy.visit(test_search_bp2_Url);
-    cy.contains( /results for:/ );
-    cy.contains( bookplate_2 );
-  });
-});
+    // click expected link
+    cy.get('uvalib-app').shadow().find('uvalib-article').shadow().find('article').find('a[href*="search"]').click();
 
-describe('Visit ' + test_search_bp3_Url, () => {
-  it('should visit ' + test_search_bp3_Url, () => { 
-    cy.visit(test_search_bp3_Url);
-    cy.contains( /results for:/ );
-    cy.contains( bookplate_3 );
-  });
-});
+    // wait
+    cy.wait(2000)
 
-describe('Visit ' + test_v4_bp1_Url, () => {
-  it('should visit ' + test_v4_bp1_Url, () => { 
-    cy.visit(test_v4_bp1_Url);
-    cy.contains( /results for:/ );
-    cy.contains( bookplate_1 );
-  });
-});
+    // make sure we have some results
+    cy.get('.hits');
 
-describe('Visit ' + test_v4_bp2_Url, () => {
-  it('should visit ' + test_v4_bp2_Url, () => { 
-    cy.visit(test_v4_bp2_Url);
-    cy.contains( /results for:/ );
-    cy.contains( bookplate_2 );
-  });
-});
-
-describe('Visit ' + test_v4_bp3_Url, () => {
-  it('should visit ' + test_v4_bp3_Url, () => { 
-    cy.visit(test_v4_bp3_Url);
-    cy.contains( /results for:/ );
-    cy.contains( bookplate_3 );
   });
 });
 
