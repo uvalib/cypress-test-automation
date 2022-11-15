1ff14ec7ecd40a8604e7b11557ef228b56787bc7
Dave Goldstein <dpg3k@virginia.edu>
2022-11-15 08:20:44 -0500

    Migrate to cypress 11.x

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  "video": false,
+  "screenshotOnRunFailure": false,
+  "defaultCommandTimeout": 30000,
+  "requestTimeout": 30000,
+  "responseTimeout": 60000,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    }
+
+  }
+});
